f6e9441ded83b699d2ad854e14a521617246a805
wszxdhr <wszxdhr@126.com>
2016-11-03 20:32:59 +0800

    初始化js，sass，编写readMe，配置Router
    包含rem.js, 添加ESLint忽略文件——rem.js，适配vuex，配置完成Router

--- README.md
+++ README.md
@@ -22,6 +22,34 @@ npm run e2e
 
 # run all tests
 npm test
+
+# install modules
+npm install node-sass sass-loader css-loader style-loader vuex vue-resource vue-router --save-dev
+
+# if you haven't sass
+Please install sass first
+
+# 已开启ESLint, 请根据自己需求关闭或调整严格程度
+
+# How To Use
+将同步书写Wiki, 有能力的童鞋欢迎关注Issues, 不胜感激
+项目初始化阶段, 后续将提供完整接口文档, 并开发后端部分, 后台部分
+
+# About Issues
+
+# About 旅梦
+南昌最有前途的前端培训课堂, 由前新浪前端工程师亲自授课, 课堂氛围活跃, 时间安排自由
+
 ```
+
+## Demo
+> 正在开发阶段, 后续会开放服务器地址
+
+## Developers
+> 小飞侠 And 潇潇
+
+## About us
+小飞侠 [个人网站](http:///)
+潇潇 [github](http:///)
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/main.js
+++ src/main.js
@@ -1,9 +1,48 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App'
+import Vuex from 'vuex'
+import store from './vuex/store'
+import rem from './js/rem'  // 引用淘宝的无限适配rem解决方案
+import common from './js/common'    // 引用通用逻辑js
+// vue components for Router
+import Hello from './components/Hello.vue'
 
+// use
+Vue.use(VueRouter)
+Vue.use(Vuex)
+rem
+common
+// use end
+
+// 1. 定义（路由）组件。
+// 可以从其他文件 import 进来
+
+// 2. 定义路由
+// 每个路由应该映射一个组件。 其中"component" 可以是
+// 通过 Vue.extend() 创建的组件构造器，
+// 或者，只是一个组件配置对象。
+// 我们晚点再讨论嵌套路由。
+const routes = [
+  { path: '/hello', component: Hello }
+]
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App }
-})
+  components: { App },
+  store,
+  router
+}).$mount('#app')
+
+// 现在，应用已经启动了！
